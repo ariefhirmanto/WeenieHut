4cb7fafc3bf5564e1531c84bf4b03db282743bbd
Abimanyu Luthfi Rizq Ramadhan <abimanyu177@gmail.com>
2025-09-11 08:55:26 +0700

    chore: add request & response post product

--- internal/server/request.go
+++ internal/server/request.go
@@ -9,3 +9,12 @@ type RegisterRequest struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required,min=8,max=32"`
 }
+
+type PostProductRequest struct {
+	Name     string `json:"name" validate:"string,required,min=4,max=32"`
+	Category string `json:"category" validate:"string,required,productType"`
+	Qty      int    `json:"qty" validate:"numeric,required,min=1"`
+	Price    int    `json:"price" validate:"numeric,required,min=100"`
+	Sku      string `json:"sku" validate:"string,required,min=0,max=32"`
+	FileID   string `json:"fileId" validate:"string,required"`
+}
