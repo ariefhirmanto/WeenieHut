d104c56bdefdacbbae3d5eb4b926099700b4e38d
meinantoyuriawan <meinantoyuriawan@gmail.com>
2025-09-11 17:58:39 +0700

    feat: method for acquiring and post the data

--- internal/server/purchase_handler.go
+++ internal/server/purchase_handler.go
@@ -2,11 +2,13 @@ package server
 
 import (
 	"WeenieHut/internal/constants"
+	"WeenieHut/internal/model"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func (s *Server) purchaseCartHandler(w http.ResponseWriter, r *http.Request) {
@@ -27,16 +29,66 @@ func (s *Server) purchaseCartHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	productSellerDummy := map[string]int{
+		"Arief Hirmanto": 1,
+		"Arief":          2,
+		"Hirmanto":       3,
+	}
+
+	seller := make(map[string]string) //{sellerID:price}
+	var products []model.ProductCart
+	var paymentdetails []model.CartPaymentDetail
+	var totalPrices int64
+	now := time.Now()
 	for _, item := range PurchaseCartRequest.PurchasedItems {
 		// query item.ProductID dari repo product, cek valid kalo bukan return 400
 		// cek quantity dari repo product, kalo kurang return 400
-		// kalo valid store ke array
 		fmt.Println(item.ProductID)
+
+		// dapet seller id dari product, harusnya dari db
+		// asumsi dapet data dari nama seller
+		sellerID := productSellerDummy["Arief Hirmanto"]
+
+		// dari productID harusnya bisa dapet seller id (atau disini user id)
+		// kalo valid store
+
+		pc := model.ProductCart{
+			ProductID:        "random",
+			Name:             "celana dalam",
+			Category:         "Clothes",
+			Qty:              999 - 1,
+			Price:            1000,
+			SKU:              "randomSKU",
+			FileID:           "randomID",
+			FileURI:          "randomURI",
+			FileThumbnailURI: "randomThumbnailUri",
+			CreatedAt:        now,
+			UpdatedAt:        now, //only keep this field, all the items beside this is come from db
+		}
+
+		_, exists := seller[sellerID]
+		if !exists {
+			seller[sellerID] = pc.price
+		} else {
+			seller[sellerID] += pc.price
+		}
+		products = append(products, pc)
+		totalPrices += pc.Price
 	}
 
-	// cek repo seller, convert sender name, type and detail ke seller id
+	// query repo seller. sender name, type and detail untuk dapet bankaccName, bankAccHolder, bankAccNum
 
+	for _, price := range seller { //should be id and price, or any distinguishable things
+		cartPayment := model.CartPaymentDetail{
+			BankAccountName:   "Arief Hirmanto",
+			BankAccountHolder: "Arief Hirmanto",
+			BankAccountNumber: "0210220234",
+			TotalPrice:        price,
+		}
+		paymentdetails = append(paymentdetails, cartPayment)
+	}
 	// kirim data ke repo purchase: array of product dari loop diatas dan seller id
+	// purchase(products, totalPrices, paymentdetails)
 	// resp := PurchaseResponse{}
 
 }
--- internal/service/purchase.go
+++ internal/service/purchase.go
@@ -2,5 +2,10 @@ package service
 
 // push data to purchase db, return purchase id. consist of purchased items {id and qty -> product table}, total price, payment details {user id -> user table}
 
-// func (s *Service) Purchase(...) (model ..., error) {
+// func (s *Service) Purchase(products []model.ProductCart, totalPrices int64, paymentDetail []model.CartPaymentDetail) (model.PurchaseCartReturn, error) {
+// yang di store
+// products.productID
+// products.qty
+// totalPrices
+// paymentDetail.semuanya
 // }
