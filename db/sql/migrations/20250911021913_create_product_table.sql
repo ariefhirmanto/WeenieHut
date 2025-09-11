e19ac392ba00fbdde7dbd41f6998fd953fb0fc02
Abimanyu Luthfi Rizq Ramadhan <abimanyu177@gmail.com>
2025-09-11 09:35:33 +0700

    chore: create migration for product table

---
+++ db/sql/migrations/20250911021913_create_product_table.sql
@@ -0,0 +1,21 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE product (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    category VARCHAR(100),                
+    qty INTEGER NOT NULL CHECK (qty >= 0),
+    price NUMERIC(12,2) NOT NULL CHECK (price >= 0),
+    sku VARCHAR(100) NOT NULL,
+    file_id VARCHAR(255),
+    file_uri TEXT,
+    file_thumbnail_uri TEXT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS product;
+-- +goose StatementEnd
