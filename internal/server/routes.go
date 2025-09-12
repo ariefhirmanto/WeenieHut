84fe78d6f63bbec7d6d0147c83d0dea81c2e4c7f
Abimanyu Luthfi Rizq Ramadhan <abimanyu177@gmail.com>
2025-09-12 21:30:01 +0700

    chore: remove unused authmiddleware on routes

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,11 +1,7 @@
 package server
 
 import (
-	"WeenieHut/internal/constants"
-	"WeenieHut/internal/utils"
-	"context"
 	"net/http"
-	"strings"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
@@ -25,33 +21,4 @@ func (s *Server) RegisterRoutes() http.Handler {
 	mux.HandleFunc("DELETE /v1/product/", s.deleteProductHandler)
 
 	return s.authMiddleware(mux)
-}
-
-func (s *Server) authMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		path := r.URL.Path
-		if path == "/health" || path == "/v1/login" || path == "/v1/register" {
-			next.ServeHTTP(w, r)
-			return
-		}
-
-		authorizationHeader := r.Header.Get("Authorization")
-		if !strings.Contains(authorizationHeader, "Bearer") {
-			sendErrorResponse(w, http.StatusUnauthorized, "unauthorized")
-			return
-		}
-
-		tokenString := strings.Replace(authorizationHeader, "Bearer ", "", -1)
-
-		userID, err := utils.ParseUserIDFromToken(tokenString)
-		if err != nil {
-			sendErrorResponse(w, http.StatusUnauthorized, "unauthorized")
-			return
-		}
-
-		ctx := context.WithValue(r.Context(), constants.UserIDCtxKey, userID)
-
-		// Proceed with the next handler
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
 }
