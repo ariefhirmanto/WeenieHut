f10c8fce0b372470a238e2e077123fd360193bc3
ardimr <rizkyardimaulana@gmail.com>
2025-09-18 23:59:45 +0700

    fix: re-apply middleware

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -25,6 +25,5 @@ func (s *Server) RegisterRoutes() http.Handler {
 	mux.HandleFunc("PUT /v1/product/", s.updateProductHandler)
 	mux.HandleFunc("DELETE /v1/product/", s.deleteProductHandler)
 
-	// return s.contentMiddleware(s.authMiddleware(mux))
-	return mux
+	return s.contentMiddleware(s.authMiddleware(mux))
 }
